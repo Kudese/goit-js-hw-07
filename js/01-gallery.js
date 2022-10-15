42922addb0cbf184b34ed83fee7f3e556691d137
Kudese <d.kuprichenkov@gmail.com>
2022-10-16 01:52:52 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const elGellery = document.querySelector(".gallery");
+
+let listGellery = "";
+let iteamGallary = ""
+galleryItems
+  .map(
+    (preview, original, description) =>
+     listGellery = listGellery +`<li><img src="${preview}" data-src="${original}" alt="${description}"></li>`
+     console.log(preview)
+  )
+  ;
+
+elGellery.innerHTML = `<ul>${listGellery}</ul>`;
