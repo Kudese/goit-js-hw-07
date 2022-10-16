4c24f556ed0ccc84dd21386e84be77a3b4443552
Kudese <d.kuprichenkov@gmail.com>
2022-10-17 02:36:27 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,29 +3,37 @@ import { galleryItems } from './gallery-items.js';
 
 // console.log(galleryItems);
 const elGallery = document.querySelector(".gallery")
+
 //Create gallary in DOM
 let gallaryList = ''
 for (let i = 0; i < galleryItems.length; i++) {
      const {preview, original, description}= galleryItems[i]
-    gallaryList += `<li>
-    <a classs='gallery__item' href='${original}' width="600px">
+    gallaryList += `<li><a classs='gallery__item' href='${original}' >
     <img class="gallery__image" src="${preview}" alt="${description}">
-    </a>
-    </li>` 
+    </a></li>` 
 }
 elGallery.innerHTML = gallaryList
 //Install simplelightbox
-let lightbox = new SimpleLightbox('.gallery a',{
-    
-});
-//Event Delegation
-elGellery.addEventListener("click", onClickImeg)
+let lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay: 250 });
 
-function onClickImeg(event) {
-    event.preventDefault();
-    //Chekc click
-    if (event.target.nodeName !== "IMG") {
-      return;
-    }
-lightbox.open()
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
