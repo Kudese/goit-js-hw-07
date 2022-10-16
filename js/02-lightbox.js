55cd197bde2dfec7957cc93967d720093e81f363
Kudese <d.kuprichenkov@gmail.com>
2022-10-17 02:50:53 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,39 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
-const elGallery = document.querySelector(".gallery")
+const elGallery = document.querySelector(".gallery");
 
 //Create gallary in DOM
-let gallaryList = ''
+let gallaryList = "";
 for (let i = 0; i < galleryItems.length; i++) {
-     const {preview, original, description}= galleryItems[i]
-    gallaryList += `<li><a classs='gallery__item' href='${original}' >
+  const { preview, original, description } = galleryItems[i];
+  gallaryList += `<li><a classs='gallery__item' href='${original}' >
     <img class="gallery__image" src="${preview}" alt="${description}">
-    </a></li>` 
+    </a></li>`;
 }
-elGallery.innerHTML = gallaryList
+elGallery.innerHTML = gallaryList;
 //Install simplelightbox
-let lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay: 250 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
